6cd958ffa30996379c2dd3009763f11f08d5b3f4
Harshit Nigdikar <harshitnigdikar@gmail.com>
2021-07-21 02:40:47 +0530

    Dealing with the basics

---
+++ my-app/src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+const button = ({title}) => (
+    <div>
+        <button className = "button">{title}</button>
+    </div>
+);
+
+export default button;
